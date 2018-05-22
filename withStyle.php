ef9aa18c5e30258c76d04707d030b59b890cb6f0
hahitafrun <37808371+hahitafrun@users.noreply.github.com>
2018-05-22 08:19:49 +0800

    Update withStyle.php
    for testing too

--- withStyle.php
+++ withStyle.php
@@ -12,7 +12,8 @@
 </head>
 <body>
 
-<h2>Hello World</h2>
+<h2 style="font-size:50px;">Hello World</h2>
+    <p style="color:red;font-size:50px;"> It is so much fun </p>
 <p class="header">London is the capital of England.</p>
 <p class="header">Paris is the capital of France.</p>
 <p class="header">Tokyo is the capital of Japan.</p>
